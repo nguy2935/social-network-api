0e57fa77133792815619dfa98d1b9216f7e62c0c
unknown <Sophia Nguyen>
2022-02-20 13:29:55 -0600

    created user controller.js and thought controller.js

---
+++ controllers/user-controller.js
@@ -0,0 +1,10 @@
+// require user model
+const {User} = require('../models');
+
+const userController = {
+    // need to create a new user
+    // get all users
+    // get a single user by the ID
+    // update the current user by ID
+    // delete a current friend
+}
