b4589dcc48d66e25a0b091f9a6162f1e4cc26a0a
unknown <Sophia Nguyen>
2022-02-20 02:19:11 -0600

    first commit. initial files setup

---
+++ models/index.js
@@ -0,0 +1,5 @@
+// require users and thoughts models
+const Users = require('.Users');
+const Thoughts = require('./Thoughts');
+
+module.exports = { Users, Thoughts };
---
+++ server.js
@@ -0,0 +1,3 @@
+// require express and mongoose
+const express = require('express');
+const mongoose = require('mongoose');
