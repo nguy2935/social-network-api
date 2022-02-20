02e20e2b5c438253e709a8376a6f6b5ab27585c4
unknown <Sophia Nguyen>
2022-02-20 13:18:41 -0600

    updated user and thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,20 +1,27 @@
-thoughtText: {
-    type: DataTypes.STRING
-    required
-    must be between 1 and 280 characters
-},
-createdAt: {
-    type: DataTypes.Date,
-    Set default value to the current timestamp
-    Use a getter method to format the timestamp on query
-},
-username (user created this thought): {
-    type: DataTypes.STRING,
-    required
-},
-reactions (these are like replies): {
-    array of nested documents created with the reaction schema
-}
+const { schema } = require('mongoose');
+
+const ThoughtSchema = new Schema( 
+    {
+        thoughtText: {
+            type: DataTypes.Data
+            required
+            must be between 1 and 280 characters
+        },
+        createdAt: {
+            type: DataTypes.Date,
+            default: Date.now,
+            get: () => .format('MM, DD, YYYY [at] hh:mm') Use a getter method to format the timestamp on query
+        },
+        username (user created this thought): {
+            type: DataTypes.STRING,
+            required: true
+        },
+        reactions (these are like replies): {
+           // array of nested documents created with the reaction schema
+        }
+    }
+)
 
 // schema settings
 // create a virtual called reactionCount that retrieves teh length of hte thought's reactions array field on query
+UserSchema.virtual('reactionCount')
--- models/User.js
+++ models/User.js
@@ -5,16 +5,16 @@ const UserSchema = new Schema(
     {
         username: {
             type: DataTypes.STRING,
-            unique
-            required
-            trimmed
+            unique: true,
+            required: true,
+            trimmed: true
             
         },
         email: {
             type: DataTypes.STRING,
-            required,
-            unique,
-            match validation
+            required: true,
+            unique: true,
+            match:
         },
         thoughts: {
             array of _id values referencing the thought model
