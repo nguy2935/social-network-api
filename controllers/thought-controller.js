2db6030c032d62b442918f8cda6d4f8aef0647ae
unknown <Sophia Nguyen>
2022-03-15 16:01:38 -0500

    fixed delete reaction to correct names

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -74,8 +74,8 @@ const thoughtController = {
         .catch(err => res.status(400).json(err));
     },
     // add a delete reaction
-        addReaction({params,body}, res) {
-            Thought.findOneAndUpdate({_id: params.thoughtId}, {$push: {reactions:body}},{new:true, runValidators:true})
+        deleteReaction({params,body}, res) {
+            Thought.findOneAndUpdate({_id: params.thoughtId}, {$pull: {reactions: {reactionId: params.reactionId}}},{new:true})
             .then(dbThoughtData => {
                 if(!dbThoughtData) {
                     res.json({message: "There is no thought with this ID!"});
