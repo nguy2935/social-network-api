e8499e743c06c2fda3a34baf4c7b291648edde6a
unknown <Sophia Nguyen>
2022-03-15 15:03:53 -0500

    routed user with user-controller

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,2 +1,17 @@
 const router = require("express").Router();
+const {
+    getAllUsers,
+    createUser,
+    getUserById,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+} = require("../../controllers/user-controller");
+
+router.route("/").get(getAllUsers).post(createUser);
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+router.route("/:userId/friends").post(addFriend);
+router.route("/:userId/:friendId").delete(deleteFriend);
+
 module.exports = router;
